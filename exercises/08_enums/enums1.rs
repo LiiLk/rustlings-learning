fb21c6815e793bf57aeec270f40ad4ea20c1b8b0
Khalil <khalil.abbioui@gmail.com>
2026-03-02 11:44:38 +0100

    Learning structs, enums and strings with rustlings

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,15 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Quit,
+    Move,
+    Echo,
+    ChangeColor,
 }
 
 fn main() {
-    println!("{:?}", Message::Resize);
+    println!("{:?}", Message::Quit);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::ChangeColor);
-    println!("{:?}", Message::Quit);
 }
